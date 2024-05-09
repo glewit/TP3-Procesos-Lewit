2e514ed5ab4db08b37d532dce67a4d2c264cf6bb
glewit <103191612+glewit@users.noreply.github.com>
2024-05-09 19:54:15 -0300

    Subo shell.c

--- src/ej2/shell.c
+++ src/ej2/shell.c
@@ -5,12 +5,24 @@
 #include <string.h>
 
 #define MAX_COMMANDS 200
+#define MAX_ARGS 128
+
+void sacar_comillas(char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        if (str[i] == '"') {
+            memmove(&str[i], &str[i + 1], strlen(str) - i);
+            i--;
+            
+        }
+    }
+}
 
 int main() {
 
     char command[256];
     char *commands[MAX_COMMANDS];
-    int command_count = 0;
+    int command_count;
 
     while (1) 
     {
@@ -30,6 +42,7 @@ int main() {
            The strtok() function breaks the command string into tokens (substrings) separated by the pipe character |. 
            In each iteration of the while loop, strtok() returns the next token found in command. 
            The tokens are stored in the commands[] array, and command_count is incremented to keep track of the number of tokens found. */
+        command_count = 0;
         char *token = strtok(command, "|");
         while (token != NULL) 
         {
@@ -37,11 +50,87 @@ int main() {
             token = strtok(NULL, "|");
         }
 
+        int pids[command_count];
+        int pipes[command_count-1][2];
+        
+        for (int i = 0; i < command_count-1; i++)
+        {
+            if (pipe(pipes[i]) == -1) {
+                perror("fallo creando pipes");
+                exit(EXIT_FAILURE);
+            }
+        }
+
         /* You should start programming from here... */
         for (int i = 0; i < command_count; i++) 
-        {
+        {      
+            char *args[MAX_ARGS];
+            int index = 0;
+            char *token_ = strtok(commands[i], " ");
+
+            while (token_ != NULL && index < MAX_ARGS - 1) {
+                sacar_comillas(token_);
+                args[index++] = token_;
+                token_ = strtok(NULL, " ");
+            }
+
+            args[index] = NULL;
+
+            // printf("Command %d arguments:\n", i);
+            for (int j = 0; j < index; j++) {
+                printf(" %s\n", args[j]);
+            }
             printf("Command %d: %s\n", i, commands[i]);
-        }    
+
+            pids[i] = fork();
+            if (pids[i] == -1){
+                perror("fallo haciendo fork");
+                exit(EXIT_FAILURE);
+            }
+
+            if(pids[i] == 0) {
+                if (i == 0) {
+                    close(pipes[i][0]);
+
+                    dup2(pipes[i][1], STDOUT_FILENO);
+                    close(pipes[i][1]);
+
+                } else if (i != command_count-1) {
+                    dup2(pipes[i-1][0], STDIN_FILENO);
+                    close(pipes[i-1][0]);
+
+                    dup2(pipes[i][1], STDOUT_FILENO);
+                    close(pipes[i][1]);
+
+                } else {
+                    close(pipes[i-1][1]);
+
+                    dup2(pipes[i-1][0], STDIN_FILENO);
+                    close(pipes[i-1][0]);
+
+                }
+
+                for (int j = 0; j < command_count - 1; j++) {
+                    if (j != i - 1) close(pipes[j][0]);
+                    if (j != i) close(pipes[j][1]);
+                }
+
+                execvp(args[0], args);
+                perror("Execution failed");
+                exit(EXIT_FAILURE);
+
+            }
+        }
+
+        for (int i = 0; i < command_count - 1; i++) {
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+        }
+
+        for (int i = 0; i < command_count; i++) {
+            waitpid(pids[i], NULL, 0);
+        }
     }
+
     return 0;
 }
